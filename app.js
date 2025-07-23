515af62d72f3f0f8d771ef9a35e0c4046acdcc06
ganesh <ganesh@gmail.com>
2025-07-23 11:32:35 +0530

    app.js-commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from GitHub Actions CI/CD'));
+app.get('/', (req, res) => res.send('Hello from Ganni GitHub Actions CI/CD Pipeline'));
 
 module.exports = app;
 
